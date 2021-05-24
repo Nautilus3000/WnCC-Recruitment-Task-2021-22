e18b22cf19785318925b9fd5802e7a5ed1747c9a
Nautilus3000 <prateekgarg.dev@protonmail.com>
2021-05-25 00:30:35 +0530

    added README.md

--- Task1/Groundworks.cpp
+++ Task1/Groundworks.cpp
@@ -36,18 +36,51 @@ struct Available_Groundworks
 };
 
 Available_Groundworks* initialize (int start_time[ ], int duration[ ], int n){
-    Available_Groundworks a;
-    a.n=n;
+    Available_Groundworks* a;
+    a->n=n;
 
     for (int i = 0; i < n; i++)
-        {   a.AvailableGw[i].Start_time=start_time[i];
-            a.AvailableGw[i].Duration=duration[i];
-            a.AvailableGw[i].End_time= start_time[i] + duration[i];
+        {   a->AvailableGw[i].Start_time=start_time[i];
+            a->AvailableGw[i].Duration=duration[i];
+            a->AvailableGw[i].End_time= start_time[i] + duration[i];
         }
-    return &a;    
+    return a;    
 
 }
 
+int CalculateMaxGroundworks(Available_Groundworks* ptr){
+    int n=ptr->n;
+    
+    int a[n];
+     int b[n];
+    for (int i = 0; i < n; i++){
+        a[i]=ptr->AvailableGw[i].Start_time;
+        b[i]= ptr->AvailableGw[i].Duration;
+    }
+   
+
+    int x=0;
+    int y[23];
+
+    for (int i = 0; i < 23; i++) {y[i]=0;}
+    for (int i = 0; i < n; i++)
+    {
+        if(b[i]==1){
+            y[a[i]]=1;
+            a[i]=0;
+            x++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if(b[i]==2 & y[a[i]] !=1 & y[a[i+1]] !=1){
+            y[a[i]]=1;
+            a[i]=0;
+            x++;
+        }
+    }
+    return x;
+}
 
 int main(){
     string names[MaxNum];
@@ -63,13 +96,16 @@ int main(){
         exit(1);                                                           // terminate with error
     }
     
-    string tempString;
 
     while (inFile >>names[n]>>start_time[n]>>duration[n]) {
         n++;
     }
     
+    cout<<CalculateMaxGroundworks(initialize(start_time,duration,n))<<endl;
+
     inFile.close();
+
+
 
     return 0;
 }
---
+++ Task1/README.md
@@ -0,0 +1,2 @@
+# Task 1
+This is which will take input from Schedule.txt and will return the max no. of them.
