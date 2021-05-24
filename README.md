6403fbe625861243d942a6aa1b2c8e942473b9e7
Nautilus3000 <prateekgarg.dev@protonmail.com>
2021-05-24 06:59:43 +0530

    Updated the Honour Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WnCC-Recruitment-Task-2021-22
-Compulsory work for the Conevenership of Web and Coding Club
+## Compulsory work for the Conevenership of Web and Coding Club
+
+_I,Prateek Garg, affirm that I have not given or received any unauthorized help on this assignment, and that this work is my own._
--- Task1/Groundworks.cpp
+++ Task1/Groundworks.cpp
@@ -1,4 +1,11 @@
 #include<iostream>
+/*******************************
+ *Author: Prateek Garg
+ *Date: 24th May 2021 
+ *Max no. Groundworks taht can be attended
+********************************/
+
+#define MaxNum 36
 
 using namespace std;
 
@@ -9,6 +16,34 @@ struct Groundworks
     int End_time;    
 };
 
+struct Available_Groundworks
+{
+    int n;                                                      //No. of Groundworks available
+    Groundworks AvailableGw[MaxNum];                            //Groundwork_Details
+                                                                
+    Available_Groundworks(){                                    //Default_Constructor
+        n= MaxNum;
+        for (int i = 0; i < n; i++)
+        {
+            AvailableGw[i].Duration=-1;
+        }
+        
+    }
+
+};
+
+Available_Groundworks* initialize (int start_time[ ], int duration[ ], int n){
+    Available_Groundworks a;
+    a.n=n;
+
+    for (int i = 0; i < n; i++)
+        {   a.AvailableGw[i].Start_time=start_time[i];
+            a.AvailableGw[i].Duration=duration[i];
+            a.AvailableGw[i].End_time= start_time[i] + duration[i];
+        }
+    return &a;    
+
+}
 
 
 int main(){
