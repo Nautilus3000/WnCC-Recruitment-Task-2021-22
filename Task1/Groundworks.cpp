d6204dcad5ee56f6544e7266962c1b4d57af3333
Nautilus3000 <prateekgarg.dev@protonmail.com>
2021-05-26 03:47:27 +0530

    final fix

--- Task1/Groundworks.cpp
+++ Task1/Groundworks.cpp
@@ -75,8 +75,9 @@ int CalculateMaxGroundworks(Available_Groundworks* ptr){
     }
     for (int i = 0; i < n; i++)
     {
-        if(b[i]==2 & y[a[i]] ==0 & y[a[i]+1] ==1){
+        if(b[i]==2 & y[a[i]] ==0 & y[a[i]+1] ==0){
             y[a[i]]=1;
+            y[a[i]+1]=1;
             a[i]=0;
             x++;
         }
